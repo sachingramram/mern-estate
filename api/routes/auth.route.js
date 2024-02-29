96f68b758eb3395e3663cbb9fdc9fa843a11a837
Sachin Pal <sachinpal9352@gmial.com>
2024-02-29 09:40:18 +0530

    complete profile page UI

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,5 +5,5 @@ const router =express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post('/google', google)
-export default router;
+router.post("/google", google)
+export default router; 
