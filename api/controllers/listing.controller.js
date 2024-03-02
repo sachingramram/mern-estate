d8a283716af7870800fe3b3c0335a6f18041c04a
Sachin Pal <sachinpal9352@gmial.com>
2024-03-02 20:15:18 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -5,7 +5,7 @@ export const createListing=async(req,res,next)=>{
     try{
         const listing=await Listing.create(req.body);
 return res.status(201).json(listing); 
-    }catch(error){
+    }catch(error){  
         next(error);
     }
 };
@@ -26,6 +26,30 @@ export const deleteListing=async(req,res,next)=>{
         next(error);
       }
     };
+     export const updateListing=async(req,res,next)=>{
+      const listing =await Listing.findById(req.params.id);
+      if(!listing){
+        return next(errorHandler(404,'Listing not found!'));
+
+      }
+      if(req.user.id!==listing.userRef){
+        return next(errorHandler(401,'You can only update your own lisitngs!'));
+
+      }
+      try{
+        const updatedListing=await Listing.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          {
+            new:true }
+        );
+        res.status(200).json(updatedListing);
+
+      }catch(error){
+        next(error);
+      }
+     }
+    
 
 
     
